5c753bf5ac409511b3cd61d7fcbea98a6b362742
Jane Luz <janerodrigues_2610@hotmail.com>
2023-04-12 11:19:25 -0300

    feat: add middleware

--- src/server.js
+++ src/server.js
@@ -1,26 +1,33 @@
 import http from 'node:http'
+import { json } from './middlewares/json.js'
 
 // para diferenciar um modulo interno do node
 
 const users = []
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
-  if (method == 'GET' && url === '/users') {
+ await json(req,res)
+
+  if (method === 'GET' && url === '/users') {
     //tranformando array em string
     return res
       .setHeader('content-type', 'application/json')
       .end(JSON.stringify(users))
   }
   if (method === 'POST' && url === '/users') {
+    const {name, email} = req.body
+
     users.push({
       id: 1,
       name: 'Janee',
-      email: 'janeteste.com'
+      email: 'janeteste.com',
+      name,
+      email,
     })
     return res.writeHead(201).end('add user')
   }
   return res.writeHead(404).end('Hello world')
 })
 
-server.listen(3334)
+server.listen(3333)

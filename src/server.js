fc3cb1d9130760a0cacd61cbc8bd34ea3b3bf308
Jane Luz <janerodrigues_2610@hotmail.com>
2023-04-13 12:24:11 -0300

    fix: add # in database

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from 'node:http'
+import { randomUUID } from 'node:crypto'
 import { Database } from './database.js'
 import { json } from './middlewares/json.js'
 
@@ -11,28 +12,26 @@ const server = http.createServer(async (req, res) => {
 
  await json(req,res)
 
-  if (method === "GET" && url === "/users") {
+  if (method === 'GET' && url === '/users') {
     //tranformando array em string
-    const users = database.select("users")
+    const users = database.select('users')
 
     return res.end(JSON.stringify(users))
   }
 
-  if (method === "POST" && url === "/users") {
+  if (method === 'POST' && url === '/users') {
     const {name, email} = req.body
 
   const user = {
-      id: 1,
-      name: "Janee",
-      email: "janeteste.com",
+      id: randomUUID(),
       name,
       email,
     }
 
-    database.insert("users",user)
-    return res.writeHead(201).end("add user")
+    database.insert('users',user)
+    return res.writeHead(201).end('add user')
   }
-  return res.writeHead(404).end("Hello world")
+  return res.writeHead(404).end('Hello world')
 })
 
 server.listen(3232)
